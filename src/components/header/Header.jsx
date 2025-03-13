ceb19d994b48da2416e7e2245dac2e6bdd12520f
yonatan <carri_2286@hotmail.com>
2025-03-12 23:28:18 -0500

    change header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -3,7 +3,7 @@ import styles from "./Header.module.css";
 function Header({ exitSession }) {
   return (
     <div className={styles.header}>
-      <p className={styles["header--title"]}>Welcome to Codeable Keep {`${localStorage.getItem("username")}`}!</p>
+      <p className={styles["header--title"]}>Welcome to project Keep {`${localStorage.getItem("username")}`}!</p>
       <button onClick={exitSession} className={styles["header--button"]}>
         Exit
       </button>
